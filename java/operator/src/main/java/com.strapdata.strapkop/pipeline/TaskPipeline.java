7552a425c4e69b6e0fc5e1b1ac52120b58438e4c
dbarthe <barthelemy.delemotte@gmail.com>
2019-07-31 14:14:19 +0200

    remove unused imports

--- java/operator/src/main/java/com.strapdata.strapkop/pipeline/TaskPipeline.java
+++ java/operator/src/main/java/com.strapdata.strapkop/pipeline/TaskPipeline.java
@@ -2,12 +2,9 @@ package com.strapdata.strapkop.pipeline;
 
 import com.squareup.okhttp.Call;
 import com.strapdata.model.Key;
-import com.strapdata.model.k8s.cassandra.DataCenter;
-import com.strapdata.model.k8s.cassandra.DataCenterList;
 import com.strapdata.model.k8s.task.Task;
 import com.strapdata.model.k8s.task.TaskList;
 import com.strapdata.strapkop.OperatorConfig;
-import com.strapdata.strapkop.cache.DataCenterCache;
 import com.strapdata.strapkop.cache.TaskCache;
 import io.kubernetes.client.ApiClient;
 import io.kubernetes.client.ApiException;
