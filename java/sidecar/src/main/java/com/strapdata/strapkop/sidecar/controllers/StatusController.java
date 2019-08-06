e578140fa2730f0be77122556ee3cf4b87a91bd4
dbarthe <barthelemy.delemotte@gmail.com>
2019-08-06 16:20:09 +0200

    Simplify and handle error in sidecar status controller

--- java/sidecar/src/main/java/com/strapdata/strapkop/sidecar/controllers/StatusController.java
+++ java/sidecar/src/main/java/com/strapdata/strapkop/sidecar/controllers/StatusController.java
@@ -6,12 +6,16 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
-import io.reactivex.Single;
 import jmx.org.apache.cassandra.service.StorageServiceMBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/status")
 @Produces(MediaType.APPLICATION_JSON)
 public class StatusController {
+    
+    private static final Logger logger = LoggerFactory.getLogger(StatusController.class);
+    
     private final StorageServiceMBean storageServiceMBean;
 
     public StatusController(CassandraModule cassandraModule) {
@@ -19,9 +23,13 @@ public class StatusController {
     }
 
     @Get("/")
-    public Single<NodeStatus> getStatus() {
-        return Single.create(emitter -> {
-            emitter.onSuccess(NodeStatus.valueOf(storageServiceMBean.getOperationMode()));
-        });
+    public NodeStatus getStatus() {
+        try {
+            return NodeStatus.valueOf(storageServiceMBean.getOperationMode());
+        }
+        catch (RuntimeException e) {
+            logger.error("error while getting operation mode", e);
+            return NodeStatus.UNKNOWN;
+        }
     }
 }
