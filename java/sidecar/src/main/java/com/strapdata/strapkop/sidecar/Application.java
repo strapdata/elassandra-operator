a0f8695de3388fd0fbe1632f40dc4667b2bc8c82
dbarthe <barthelemy.delemotte@gmail.com>
2019-08-07 19:34:26 +0200

    delay sidecar startup

--- java/sidecar/src/main/java/com/strapdata/strapkop/sidecar/Application.java
+++ java/sidecar/src/main/java/com/strapdata/strapkop/sidecar/Application.java
@@ -5,6 +5,12 @@ import io.micronaut.runtime.Micronaut;
 public class Application {
 
     public static void main(String[] args) {
+        // wait 30 sec so that elassandra has time to start
+        try {
+            Thread.sleep(30 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         Micronaut.run(Application.class);
     }
 }
