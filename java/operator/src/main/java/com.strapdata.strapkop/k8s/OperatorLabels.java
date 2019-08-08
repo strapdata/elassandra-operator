e2d4965a19c1ebb4a5effd16336a6a18c753847d
dbarthe <barthelemy.delemotte@gmail.com>
2019-08-08 20:09:35 +0200

    Make labels compatible with existing grafana dashboard

--- java/operator/src/main/java/com.strapdata.strapkop/k8s/OperatorLabels.java
+++ java/operator/src/main/java/com.strapdata.strapkop/k8s/OperatorLabels.java
@@ -10,20 +10,30 @@ import java.util.Map;
 public final class OperatorLabels {
     public static final String POD = "statefulset.kubernetes.io/pod-name";
     public static final String ZONE = "failure-domain.beta.kubernetes.io/zone";
+
     
-    public static final String PARENT = "elassandra-operator.strapdata.com/parent"; // parent datacenter resource name
-    public static final String CLUSTER = "elassandra-operator.strapdata.com/cluster";
-    public static final String DATACENTER = "elassandra-operator.strapdata.com/datacenter";
-    public static final String RACK = "elassandra-operator.strapdata.com/rack";
+    // public static final String labelPrefix = "elassandra-operator.strapdata.com/";
+    // no prefix for compatibility with vroyer's grafana dashboard
+    public static final String labelPrefix = "";
     
-    public static final String DATACENTER_GENERATION = "elassandra-operator.strapdata.com/datacenter-generation";
+    
+    public static final String PARENT = labelPrefix + "parent"; // parent datacenter resource name
+    public static final String CLUSTER = labelPrefix + "cluster";
+    public static final String DATACENTER = labelPrefix + "datacenter";
+    public static final String RACK = labelPrefix + "rack";
+    
+    // TODO: this is confusing with helm "release" label
+    public static final String RELEASE = labelPrefix + "release";
+    
+    public static final String DATACENTER_GENERATION = labelPrefix + "datacenter-generation";
     
     // this annotation is used to store a hash of the config map in the pod so that statefulset trigger a rolling restart
     // when the config map change
-    public static final String CONFIGMAP_FINGERPRINT = "elassandra-operator.strapdata.com/configmap-fingerprint";
+    public static final String CONFIGMAP_FINGERPRINT = labelPrefix + "configmap-fingerprint";
     
     public static final Map<String, String> MANAGED = ImmutableMap.of(
-            "app.kubernetes.io/managed-by", "elassandra-operator"
+            "app.kubernetes.io/managed-by", "elassandra-operator",
+            "app", "elassandra" // compatibility with dashboard
     );
 
     private OperatorLabels() {}
@@ -59,6 +69,7 @@ public final class OperatorLabels {
         return ImmutableMap.<String, String>builder()
                 .putAll(rack(dataCenter, rackName))
                 .put(POD, podName)
+                .put(RELEASE, dataCenter.getSpec().getElassandraImage())
                 .build();
     }
     
