86ba9e883f20f47cf82f3f7a26136579559457b7
dbarthe <barthelemy.delemotte@gmail.com>
2019-08-21 10:11:50 +0200

    remove cqlconnection after deleting dc

--- java/operator/src/main/java/com.strapdata.strapkop/reconcilier/DataCenterDeleteReconcilier.java
+++ java/operator/src/main/java/com.strapdata.strapkop/reconcilier/DataCenterDeleteReconcilier.java
@@ -1,6 +1,7 @@
 package com.strapdata.strapkop.reconcilier;
 
 import com.strapdata.model.k8s.cassandra.DataCenter;
+import com.strapdata.strapkop.cql.CqlConnectionManager;
 import io.micronaut.context.ApplicationContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,9 +14,11 @@ public class DataCenterDeleteReconcilier extends Reconcilier<DataCenter> {
     private final Logger logger = LoggerFactory.getLogger(DataCenterDeleteReconcilier.class);
     
     private final ApplicationContext context;
+    private final CqlConnectionManager cqlConnectionManager;
     
-    public DataCenterDeleteReconcilier(final ApplicationContext context) {
+    public DataCenterDeleteReconcilier(final ApplicationContext context, CqlConnectionManager cqlConnectionManager) {
         this.context = context;
+        this.cqlConnectionManager = cqlConnectionManager;
     }
     
     @Override
@@ -24,6 +27,8 @@ public class DataCenterDeleteReconcilier extends Reconcilier<DataCenter> {
         try {
             logger.debug("processing a dc delete reconciliation for {} in thread {}", dc.getMetadata().getName(), Thread.currentThread().getName());
             context.createBean(DataCenterDeleteAction.class, dc).deleteDataCenter();
+            
+            cqlConnectionManager.removeConnection(dc);
         }
         catch (Exception e) {
             logger.error("an error occurred while processing DataCenter update reconciliation for {}", dc.getMetadata().getName(), e);
