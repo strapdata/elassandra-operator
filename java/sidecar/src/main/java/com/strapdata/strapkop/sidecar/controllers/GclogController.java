fa7297bd754a0662917bb6aa04d6a831879c010a
vroyer <vroyer@strapdata.com>
2019-09-30 21:00:16 +0200

    Add env var for GC logdir

--- java/sidecar/src/main/java/com/strapdata/strapkop/sidecar/controllers/GclogController.java
+++ java/sidecar/src/main/java/com/strapdata/strapkop/sidecar/controllers/GclogController.java
@@ -4,6 +4,8 @@ package com.strapdata.strapkop.sidecar.controllers;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.reactivex.Single;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.Arrays;
@@ -17,10 +19,15 @@ import java.util.stream.Collectors;
 @Controller("/gclog")
 public class GclogController {
 
+    private static final Logger logger = LoggerFactory.getLogger(GclogController.class);
+
     File gclogDir;
 
     public GclogController() {
-        gclogDir = new File("/tmp/");
+        gclogDir = new File(System.getenv("GCLOG_DIR") == null ? "/var/log/cassandra" : System.getenv("GCLOG_DIR"));
+        if (!gclogDir.exists()) {
+            logger.error("GC log directory does not exists:"+gclogDir.getAbsolutePath());
+        }
     }
 
     @Get("/")
