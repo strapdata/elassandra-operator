1411cde8d93def0b0adba7b0cfbe4216b3d2e1c4
vroyer <vroyer@strapdata.com>
2019-10-06 15:53:47 +0200

    Add operator swagger annotations

--- java/operator/src/main/java/com/strapdata/strapkop/Application.java
+++ java/operator/src/main/java/com/strapdata/strapkop/Application.java
@@ -1,7 +1,22 @@
 package com.strapdata.strapkop;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Strapkop operator",
+                version = "0.1",
+                description = "Strapdata Elassandra operator",
+                license = @License(name = "Strapdata license", url = "http://petstore.notreal")
+        ),
+        tags = {
+                @Tag(name = "seeds")
+        }
+)
 public class Application {
     public static void main(String[] args) {
         Micronaut.run(Application.class);
