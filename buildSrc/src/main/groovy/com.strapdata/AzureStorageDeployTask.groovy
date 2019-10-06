96decf4338fc4970959c7c623cb8bf8c44fefa61
vroyer <vroyer@strapdata.com>
2019-10-06 17:48:32 +0200

    Add gradle license report and upload tasks

---
+++ buildSrc/src/main/groovy/com.strapdata/AzureStorageDeployTask.groovy
@@ -0,0 +1,49 @@
+package com.strapdata
+
+import com.microsoft.azure.storage.CloudStorageAccount
+import com.microsoft.azure.storage.blob.CloudBlobClient
+import com.microsoft.azure.storage.blob.CloudBlobContainer
+import com.microsoft.azure.storage.blob.CloudBlockBlob
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class AzureStorageDeployTask extends DefaultTask {
+
+    String connectionString
+    String container
+    String fileToDeploy
+    String mimeType
+
+    @TaskAction
+    def deploy() {
+        checkArguments()
+
+        print "connectionString=${connectionString}"
+        CloudStorageAccount account = CloudStorageAccount.parse(connectionString);
+        CloudBlobClient serviceClient = account.createCloudBlobClient();
+
+        CloudBlobContainer blobContainer = serviceClient.getContainerReference(container);
+        blobContainer.createIfNotExists();
+
+        def file = project.file(fileToDeploy)
+
+        CloudBlockBlob blob = blobContainer.getBlockBlobReference(file.name);
+        if (mimeType)
+            blob.getProperties().setContentType(mimeType);
+        blob.upload(new FileInputStream(file), file.length());
+    }
+
+    private void checkArguments() {
+        if (!connectionString) {
+            throw new IllegalArgumentException("The 'connectionString' property is missing");
+        }
+
+        if (!container) {
+            throw new IllegalArgumentException("The 'container' property is missing");
+        }
+
+        if (!fileToDeploy) {
+            throw new IllegalArgumentException("The 'fileToDeploy' property is missing");
+        }
+    }
+}
