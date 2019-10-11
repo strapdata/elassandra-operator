264d598b6228b2ab6c6f8220475e418409e7d796
vroyer <vroyer@strapdata.com>
2019-10-11 13:53:20 +0200

    Add JMXMP support

--- java/sidecar/src/main/java/com/strapdata/strapkop/sidecar/cassandra/CassandraConfiguration.java
+++ java/sidecar/src/main/java/com/strapdata/strapkop/sidecar/cassandra/CassandraConfiguration.java
@@ -4,7 +4,9 @@ import io.micronaut.context.annotation.ConfigurationProperties;
 
 @ConfigurationProperties("cassandra_jmx")
 public class CassandraConfiguration {
-    String jmxServiceURL = "service:jmx:rmi:///jndi/rmi://"+System.getenv("POD_IP")+":" + System.getenv("JMX_PORT") + "/jmxrmi";
+    String jmxServiceURL = System.getProperty("cassandra.jmxmp") != null ?
+            "service:jmx:jmxmp://" + System.getenv("POD_IP") + ":" + System.getenv("JMX_PORT") + "/" :
+            "service:jmx:rmi:///jndi/rmi://" + System.getenv("POD_IP") + ":" + System.getenv("JMX_PORT") + "/jmxrmi";
 
     String jmxUsername = "cassandra";
 
