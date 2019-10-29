a40f3df80e74b6446f1ce34217bc66d3e4c0fe47
vroyer <vroyer@strapdata.com>
2019-10-29 12:32:26 +0100

    dump write cache settings

--- docker/systune.sh
+++ docker/systune.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# dump dick write cache settings
+# Access timestamps disabled, and other options depending on the FS
+# vm.dirty_ratio = 80 # from 40 $
+# vm.dirty_background_ratio = 5 # from 10
+# vm.dirty_expire_centisecs = 12000 # from 3000
+# mount -o defaults,noatime,discard,nobarrier …
+sysctl -a | grep dirty
+
 # memlock unlimited
 ulimit -l unlimited
 
