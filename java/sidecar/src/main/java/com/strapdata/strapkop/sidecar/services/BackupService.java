0dbe93a013965949a71fd560928c72ae3a424ef2
leleueri <eric.leleu.dev@gmail.com>
2019-12-09 11:42:36 +0100

    [Backup] Take Snaphost synchronously

--- java/sidecar/src/main/java/com/strapdata/strapkop/sidecar/services/BackupService.java
+++ java/sidecar/src/main/java/com/strapdata/strapkop/sidecar/services/BackupService.java
@@ -2,6 +2,7 @@ package com.strapdata.strapkop.sidecar.services;
 
 import com.instaclustr.backup.task.BackupTask;
 import com.instaclustr.backup.util.GlobalLock;
+import com.microsoft.azure.storage.StorageException;
 import com.strapdata.model.backup.BackupArguments;
 import io.micronaut.context.annotation.Context;
 import io.micronaut.context.annotation.Infrastructure;
@@ -12,6 +13,11 @@ import jmx.org.apache.cassandra.service.StorageServiceMBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.naming.ConfigurationException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
+
 @Context
 @Infrastructure
 public class BackupService {
@@ -20,19 +26,18 @@ public class BackupService {
     
     private final StorageServiceMBean storageServiceMBean;
     
-    private BehaviorSubject<BackupArguments> subject = BehaviorSubject.create();
+    private BehaviorSubject<BackupTask> subject = BehaviorSubject.create();
     private Disposable disposable;
     
     public BackupService(StorageServiceMBean storageServiceMBean) {
         this.storageServiceMBean = storageServiceMBean;
         logger.info("Initializing BackupService");
         disposable = subject.observeOn(Schedulers.newThread())
-                .doOnNext(backupArguments -> logger.info("received backup request for {}", backupArguments.snapshotTag))
-                .doOnNext(backupArguments -> logger.debug("processing backup on thread {}", Thread.currentThread().getName()))
-                .map(backupArguments -> new BackupTask(backupArguments, new GlobalLock("/tmp"), this.storageServiceMBean))
+                .doOnNext(task -> logger.info("Start backup upload for snapshot '{}'", task.getArguments().snapshotTag))
+                .doOnNext(task -> logger.debug("processing backup upload for snapshot '{}' on thread {}", task.getArguments().snapshotTag, Thread.currentThread().getName()))
                 .doOnNext(task -> {
                     try {
-                        task.call();
+                        task.performBackup();
                     }
                     catch (Throwable throwable) {
                         logger.error("error while processing backup", throwable);
@@ -46,7 +51,24 @@ public class BackupService {
     }
     
     public void enqueueBackup(final BackupArguments arguments) {
-        subject.onNext(arguments);
+        // Create the BackupTask
+        logger.info("received backup request for {}", arguments.snapshotTag);
+        BackupTask task;
+        GlobalLock globalLock;
+        try {
+            globalLock = new GlobalLock("/tmp");
+            task = new BackupTask(arguments, globalLock, this.storageServiceMBean);
+            globalLock.getLock(arguments.waitForLock);
+        } catch (IOException|StorageException|ConfigurationException|URISyntaxException|InvalidKeyException e) {
+            logger.error("Unable to create BackupTask : {} ", e.getMessage(), e);
+            return;
+        }
+
+        // snapshot in synchronous manner and then backup if snapshot succeeded
+        if (task.performSnapshot()) {
+            // submit the task to upload the SSTable in async manner
+            subject.onNext(task);
+        }
     }
 
     // TODO: bind stop event to gracefully shutdown backups
