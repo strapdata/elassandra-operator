faa5f48a3e3454f6666435464337eb8b2940c02e
leleueri <eric.leleu.dev@gmail.com>
2020-01-27 17:06:55 +0100

    [Ingress] create a n ingress for ElasticSearch service

---
+++ java/operator/src/main/java/com/strapdata/strapkop/k8s/ServicesConstants.java
@@ -0,0 +1,9 @@
+package com.strapdata.strapkop.k8s;
+
+public interface ServicesConstants {
+    String ELASTICSEARCH_PORT_NAME = "elasticsearch";
+    int ELASTICSEARCH_PORT_VALUE = 9200;
+
+    String PROMETHEUS_SERVICE_NAME = "prometheus";
+    int PROMETHEUS_SERVICE_PORT = 9500;
+}
