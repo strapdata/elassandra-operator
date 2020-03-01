41277f7c64e2df124ab96a319dd750a49e5cc58e
vroyer <vroyer@strapdata.com>
2020-03-01 23:56:26 +0100

    Keep the license for later checks (TODO: purge en delete)

--- java/operator/src/main/java/com/strapdata/strapkop/cql/CqlLicenseManager.java
+++ java/operator/src/main/java/com/strapdata/strapkop/cql/CqlLicenseManager.java
@@ -6,7 +6,6 @@ import com.datastax.driver.core.exceptions.AuthenticationException;
 import com.datastax.driver.core.exceptions.NoHostAvailableException;
 import com.strapdata.elasticsearch.plugin.license.License;
 import com.strapdata.elasticsearch.plugin.license.LicenseVerifierService;
-import com.strapdata.strapkop.exception.InvalidLicenseException;
 import com.strapdata.strapkop.model.k8s.cassandra.DataCenter;
 import io.reactivex.Completable;
 import io.reactivex.Single;
@@ -21,26 +20,34 @@ import java.util.stream.StreamSupport;
 @Singleton
 public class CqlLicenseManager extends AbstractManager<License> implements LicenseVerifierService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CqlLicenseManager.class);
-    public static final String SELECT_STATEMENT = "SELECT * from elastic_admin.licenses";
+    private static final Logger logger = LoggerFactory.getLogger(CqlLicenseManager.class);
+    private static final String SELECT_STATEMENT = "SELECT * from elastic_admin.licenses";
+
+    public static final String LICENSE_KEY = "license";
 
     public Completable verifyLicense(DataCenter dataCenter, CqlSessionHandler sessionHandler) {
         return Single.just(sessionHandler)
                 .flatMap(handler -> sessionHandler.getSession(dataCenter))
                 .map(session -> {
                     try {
-                        LOGGER.trace(SELECT_STATEMENT);
+                        logger.trace(SELECT_STATEMENT);
                         ResultSet rs = session.execute(session.prepare(SELECT_STATEMENT).setConsistencyLevel(ConsistencyLevel.LOCAL_QUORUM).bind());
                         Stream<License> rowStream = StreamSupport.stream(rs.spliterator(), false).map(CqlLicense::fromRow);
 
                         License license = verifyLicenses(rowStream, dataCenter.getSpec().getClusterName(), dataCenter.getSpec().getDatacenterName());
-                        LOGGER.trace("datacenter={} license={}", dataCenter.id(), license);
-                        if (license.isExpired())
-                            throw new InvalidLicenseException("License '"+license.getId()+"' has expired");
-
+                        logger.debug("datacenter={} license={}", dataCenter.id(), license);
+                        if (license == null) {
+                            logger.warn("datacenter={} No license found", dataCenter.id());
+                            return Optional.empty();
+                        }
+                        if (license.isExpired()) {
+                            logger.warn("datacenter={} Expired license={}", dataCenter.id(), license);
+                            return Optional.empty();
+                        }
+                        addIfAbsent(dataCenter, LICENSE_KEY, () -> license);
                         return Optional.ofNullable(license);
                     } catch(AuthenticationException | NoHostAvailableException e) {
-                        LOGGER.warn("datacenter="+dataCenter.id()+" Unable to get the Elassandra License", e);
+                        logger.warn("datacenter="+dataCenter.id()+" Unable to get the Elassandra License", e);
                         return Optional.empty();
                     }
                 }).ignoreElement();
