97a14454136c3475714ca58eb2afc571e66973ee
vroyer <vroyer@strapdata.com>
2020-05-04 18:35:02 +0200

    Move CRD manifests in model module

--- java/model/src/main/java/com/strapdata/strapkop/model/k8s/StrapdataCrdGroup.java
+++ java/model/src/main/java/com/strapdata/strapkop/model/k8s/StrapdataCrdGroup.java
@@ -1,5 +1,15 @@
 package com.strapdata.strapkop.model.k8s;
 
-public interface StrapdataCrdGroup {
-    public final String GROUP = "stable.strapdata.com";
+import java.io.InputStream;
+
+public class StrapdataCrdGroup {
+    public static final String GROUP = "stable.strapdata.com";
+
+    public InputStream getDataCenterCrd() {
+        return StrapdataCrdGroup.class.getResourceAsStream("/datacenter-crd.yaml");
+    }
+
+    public InputStream getTaskCrd() {
+        return StrapdataCrdGroup.class.getResourceAsStream("/task-crd.yaml");
+    }
 }
