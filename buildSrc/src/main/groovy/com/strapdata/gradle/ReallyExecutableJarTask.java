bf75e8cfd017d09b013c7d5b1b29fd87428d02bb
vroyer <vroyer@strapdata.com>
2020-05-26 10:00:31 +0200

    Make edctl really executable

--- buildSrc/src/main/groovy/com/strapdata/gradle/ReallyExecutableJarTask.java
+++ buildSrc/src/main/groovy/com/strapdata/gradle/ReallyExecutableJarTask.java
@@ -10,7 +10,6 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ReallyExecutableJarTask extends org.gradle.api.DefaultTask {
@@ -19,7 +18,8 @@ public class ReallyExecutableJarTask extends org.gradle.api.DefaultTask {
     public void run() {
         try {
             Project project = getProject();
-            File file = new File(String.format("%s/build/libs/%s-%s.jar", project.getName(), project.getName(), project.getVersion()));
+            File jarfile = new File(String.format("%s/libs/%s.jar", project.getBuildDir(), project.getName()));
+            File exefile = new File(String.format("%s/libs/%s", project.getBuildDir(), project.getName()));
 
             /**
              * Shell script to add to the jar instead of the default stanza.
@@ -31,12 +31,10 @@ public class ReallyExecutableJarTask extends org.gradle.api.DefaultTask {
              */
             String flags = "-Dmicronaut.environments=$MICRONAUT_ENV";
 
-            System.out.println("Making " + file.toPath() + " executable");
+            System.out.println("Making " + exefile.toPath() + " executable");
 
-            Path original = Paths.get(file.toPath() + ".rx-orig");
-            Files.move(file.toPath(), original);
-            try (final FileOutputStream out = new FileOutputStream(file);
-                 final InputStream in = Files.newInputStream(original)) {
+            try (final FileOutputStream out = new FileOutputStream(exefile);
+                 final InputStream in = Files.newInputStream(jarfile.toPath())) {
 
                 if (scriptFile == null) {
                     out.write(("#!/bin/sh\n\nexec java " + flags + " -jar \"$0\" \"$@\"\n\n").getBytes("ASCII"));
@@ -47,9 +45,9 @@ public class ReallyExecutableJarTask extends org.gradle.api.DefaultTask {
                     out.write(script);
                     out.write(new byte[]{'\n', '\n'});
                 } else {
-                    System.out.println(String.format("Loading file[%s] from jar[%s]", scriptFile, original));
+                    System.out.println(String.format("Loading file[%s] from jar[%s]", scriptFile, jarfile));
 
-                    try (final URLClassLoader loader = new URLClassLoader(new URL[]{original.toUri().toURL()}, null);
+                    try (final URLClassLoader loader = new URLClassLoader(new URL[]{jarfile.toPath().toUri().toURL()}, null);
                          final InputStream scriptIn = loader.getResourceAsStream(scriptFile)) {
 
                         out.write(IOUtil.toString(scriptIn).getBytes("ASCII"));
@@ -57,13 +55,9 @@ public class ReallyExecutableJarTask extends org.gradle.api.DefaultTask {
                     }
                 }
                 IOUtil.copy(in, out);
-            } finally {
-                Files.deleteIfExists(original);
             }
-
-            file.setExecutable(true, false);
-
-            System.out.println(String.format("Successfully made JAR [%s] executable", file.getAbsolutePath()));
+            exefile.setExecutable(true, false);
+            System.out.println(String.format("Successfully made JAR [%s] executable", exefile.getAbsolutePath()));
         } catch (Exception e) {
             System.err.println("error:" + e.getMessage());
             e.printStackTrace(System.err);
