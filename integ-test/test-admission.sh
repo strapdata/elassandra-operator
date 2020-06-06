e93bdcdaee956186af8f9c693b982eaa2c3c831b
vroyer <vroyer@strapdata.com>
2020-06-06 13:35:28 +0200

    Update test-admission.sh

--- integ-test/test-admission.sh
+++ integ-test/test-admission.sh
@@ -6,16 +6,17 @@ setup_flavor
 NS="ns3"
 
 install_elassandra_datacenter $NS cl1 dc1 1
+sleep 2
 
 # cannot change the cluster name
-kubectl patch edc elassandra-cl1-dc1 -n $NS --type="merge" --patch '{"spec": {"clusterName": "cl2" }}'
+kubectl patch elassandradatacenter elassandra-cl1-dc1 -n $NS --type="merge" --patch '{"spec": {"clusterName": "cl2" }}'
 if [ $? -eq 0 ]; then
   echo "Webhook admission should refuse cluster name change"
   finish
 fi
 
 # cannot change the datacenter name
-kubectl patch edc elassandra-cl1-dc1 -n $NS --type="merge" --patch '{"spec": {"datacenterName": "dc2" }}'
+kubectl patch elassandradatacenter elassandra-cl1-dc1 -n $NS --type="merge" --patch '{"spec": {"datacenterName": "dc2" }}'
 if [ $? -eq 0 ]; then
   echo "Webhook admission should refuse datacenter name change"
   finish
