68154c2120582b62021d31e4ffa64d518e298e48
vroyer <vroyer@strapdata.com>
2020-06-16 09:46:18 +0200

    Update test-replace-pvc.sh

--- integ-test/test-replace-pvc.sh
+++ integ-test/test-replace-pvc.sh
@@ -15,21 +15,24 @@ java/edctl/build/libs/edctl watch-dc -n elassandra-cl1-dc1 -ns $NS --health GREE
 kubectl exec -it elassandra-cl1-dc1-0-0 -n $NS -- cqlsh -e "CREATE KEYSPACE foo WITH replication = {'class': 'NetworkTopologyStrategy','dc1':'2'};"
 kubectl exec -it elassandra-cl1-dc1-0-0 -n $NS -- bash -l -c "for i in {1..$N}; do post foo/bar '{\"foo\":\"bar\"}'; done"
 
+# flush to stream SSTables
 kubectl exec -it elassandra-cl1-dc1-0-0 -n $NS -- bash -l -c 'nodetool $NODETOOL_OPTS -p $NODETOOL_JMX_PORT flush'
 kubectl exec -it elassandra-cl1-dc1-1-0 -n $NS -- bash -l -c 'nodetool $NODETOOL_OPTS -p $NODETOOL_JMX_PORT flush'
 kubectl exec -it elassandra-cl1-dc1-2-0 -n $NS -- bash -l -c 'nodetool $NODETOOL_OPTS -p $NODETOOL_JMX_PORT flush'
 
-# remove PVC protection for test
+# delete PVC
+kb get pvc -n $NS
 PVC=data-volume-elassandra-cl1-dc1-0-0
-kubectl delete pvc $PVC --force --grace-period=0 &
-kubectl patch pvc $PVC -p '{"metadata":{"finalizers": []}}' --type=merge
+kubectl delete pvc $PVC -n $NS --force --grace-period=0 &
+kubectl patch pvc $PVC -n $NS -p '{"metadata":{"finalizers": []}}' --type=merge
 
 # delete pod to restart with an empty disk
-kubectl delete pod elassandra-cl1-dc1-0-0
+kubectl delete pod -n $NS elassandra-cl1-dc1-0-0
 java/edctl/build/libs/edctl watch-dc -n elassandra-cl1-dc1 -ns $NS --health GREEN -r 3
 kubectl exec -it elassandra-cl1-dc1-0-0 -n $NS -- bash -l -c "post _updaterouting"
 sleep 5
 
+# check we properly recover
 TOTAL_HIT=$(kubectl exec -it elassandra-cl1-dc1-0-0 -n $NS -- bash -l -c "get 'foo/bar/_search?pretty'" | tail -n +4 | jq ".hits.total")
 if [ "$TOTAL_HIT" != "$N" ]; then
    echo "Error, expecting $N docs in foo on dc1"
