080a148dfd21c41a1f58b0f4b625eb559bf4e9c4
vroyer <vroyer@strapdata.com>
2020-06-19 09:21:25 +0200

    Fix AKS

--- integ-test/aks/aks.sh
+++ integ-test/aks/aks.sh
@@ -12,32 +12,31 @@ export K8S_CLUSTER_NAME=${K8S_CLUSTER_NAME:-"cluster1"}
 
 export REGISTRY_NAME=strapdata
 export REGISTRY_URL=$REGISTRY_NAME.azurecr.io
-export REGISTRY_SECRET_NAME=""                # Registry is attached to AKS cluster
 
-function create_cluster() {
+create_cluster() {
   create_resource_group $RESOURCE_GROUP_NAME
   create_aks_cluster
 }
 
-function delete_cluster() {
+delete_cluster() {
   delete_aks_cluster $RESOURCE_GROUP_NAME $K8S_CLUSTER_NAME
   delete_resource_group $RESOURCE_GROUP_NAME
 }
 
 
 # $1 = $RESOURCE_GROUP_NAME
-function create_resource_group() {
+create_resource_group() {
   az group create -l westeurope -n $1
 }
 
 # $1 = $RESOURCE_GROUP_NAME
-function delete_resource_group() {
+delete_resource_group() {
     az group delete -n $1
 }
 
 # $1 = $RESOURCE_GROUP_NAME
 # $1 = vnet name
-function create_vnet0() {
+create_vnet0() {
   az network vnet create --name vnet0 -g $1 --address-prefix 10.0.0.0/17 --subnet-name subnet0 --subnet-prefix 10.0.0.0/24
 }
 
@@ -50,18 +49,14 @@ create_azure_registry() {
   az acr create --name $REGISTRY_NAME --resource-group $RESOURCE_GROUP_NAME --sku Basic
 }
 
-function create-acr-rbac() {
+create-acr-rbac() {
     eval $(az ad sp create-for-rbac \
     --scopes /subscriptions/$SUBSCRIPTION_ID/resourcegroups/$ACR_RESOURCE_GROUP/providers/Microsoft.ContainerRegistry/registries/$ACR_NAME \
     --role Reader \
     --name $SERVICE_PRINCIPAL_NAME | jq -r '"export SPN_PW=\(.password) && export SPN_CLIENT_ID=\(.appId)"')
 }
 
-function create-acr-secret() {
-    kubectl create secret docker-registry $REGISTRY_SECRET_NAME --docker-server=https://$REGISTRY_URL --docker-username="$SPN_CLIENT_ID" --docker-password="$SPN_PW" --docker-email="vroyer@strapdata.com"
-}
-
-function create_aks_cluster() {
+create_aks_cluster() {
      az aks create --name "${K8S_CLUSTER_NAME}" \
                   --resource-group $RESOURCE_GROUP_NAME \
                   --network-plugin azure \
@@ -83,7 +78,7 @@ function create_aks_cluster() {
 # AKS zone availability (require VM Scale Set) does not allow to add public IPs on nodes because of the standard LB.
 # So, keep AvailabilitySet deployment with no LB unless you deploy one.
 # $1 = k8s cluster IDX
-function create_aks_cluster_advanced() {
+create_aks_cluster_advanced() {
      az network vnet subnet create -g $RESOURCE_GROUP_NAME --vnet-name vnet0 -n "subnet$1" --address-prefixes 10.0.$1.0/24
      local B3=$((64+($1 -1)*16))
 
@@ -109,7 +104,7 @@ function create_aks_cluster_advanced() {
 
 # $1 = k8s cluster IDX
 # $2 = node index
-function add_public_ip() {
+add_public_ip() {
    AKS_RG_NAME=$(az resource show --namespace Microsoft.ContainerService --resource-type managedClusters -g $RESOURCE_GROUP_NAME -n "${K8S_CLUSTER_NAME}${1}" | jq -r .properties.nodeResourceGroup)
    AKS_NODE=$(az vm list --resource-group MC_koptest2_kube2_westeurope | jq -r ".[$2] .name")
    az network nic ip-config list --nic-name "${AKS_NODE::-2}-nic-2" -g $AKS_RG_NAME
@@ -119,7 +114,7 @@ function add_public_ip() {
 
 # require AKS with VM ScaleSet
 # $1 = k8s cluster IDX
-function add_nodepool2() {
+add_nodepool2() {
     az aks nodepool add --cluster-name "${K8S_CLUSTER_NAME}${1}" \
                   --resource-group $RESOURCE_GROUP_NAME \
                   --name nodepool2 \
@@ -130,14 +125,13 @@ function add_nodepool2() {
 
 # $1 = RESOURCE_GROUP_NAME
 # $2 = K8S_CLUSTER_NAME
-function delete_aks_cluster() {
+delete_aks_cluster() {
     az aks delete  --name "${2}" --resource-group $1
 }
 
 # $1 = RESOURCE_GROUP_NAME
 # $2 = K8S_CLUSTER_NAME
-function use_aks_cluster() {
+use_aks_cluster() {
 	 az aks get-credentials --name "${2}" --resource-group $1 --output table
 	 kubectl config set-context $1 --cluster=${2}
-}
 }
