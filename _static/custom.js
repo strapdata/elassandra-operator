862d2860e225548b2ec46c041f51e1cc50190cdb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-29 21:05:35 +0000

    Deploy operator.elassandra.io to github.com/strapdata/elassandra-operator.git:gh-pages

---
+++ _static/custom.js
@@ -0,0 +1,4 @@
+ga('create', 'UA-169624673-1', 'auto', {allowLinker: true});
+ga('require', 'linker');
+ga('send', 'pageview');
+ga('linker:autoLink', ['www.elassandra.io', 'strapdata.com', 'operator.elassandra.io']);
