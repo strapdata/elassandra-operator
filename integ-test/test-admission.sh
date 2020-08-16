842a1d047a963a508b4e0bbbf6b8869dcfd8ff80
vroyer <vroyer@strapdata.com>
2020-08-16 19:31:52 +0200

    Update test-admission.sh

--- integ-test/test-admission.sh
+++ integ-test/test-admission.sh
@@ -29,8 +29,9 @@ if [ $? -eq 0 ]; then
   error
 fi
 
-ELASSANDRA_OPERATOR_POD=$(kubectl get pod -l app=elassandra-operator  -o custom-columns=NAME:.metadata.name --no-headers)
-SEED_IP=$(kubectl exec $ELASSANDRA_OPERATOR_POD -- curl -k "https://localhost/seeds/$NS/cl1/dc1" 2>/dev/null | jq -r ".[0]")
+ELASSANDRA_OPERATOR_PORT=$(kubectl get service elassandra-operator -n default -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
+ELASSANDRA_OPERATOR_POD=$(kubectl get pod -n default -l app=elassandra-operator  -o custom-columns=NAME:.metadata.name --no-headers)
+SEED_IP=$(kubectl exec $ELASSANDRA_OPERATOR_POD -n default -- curl -k "https://localhost:$ELASSANDRA_OPERATOR_PORT/seeds/$NS/cl1/dc1" 2>/dev/null | jq -r ".[0]")
 if [ -z "$SEED_IP" ]; then
   echo "Seed IP is empty"
   error
